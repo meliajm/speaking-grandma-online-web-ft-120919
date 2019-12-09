2f37a03625d20b3a9e1b731e541a5bbdcfb3b909
melia miller <meliajm@gmail.com>
2019-12-09 19:24:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,11 @@
 
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
-    ""
+    "I LOVE YOU TOO PUMPKIN!"
   elsif string != string.upcase
     "HUH?! SPEAK UP, SONNY!"
+  else
+    "NO, NOT SINCE 1938"
   
   end 
   
